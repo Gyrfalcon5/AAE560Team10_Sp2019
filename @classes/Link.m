94a253a39e52070f22b5a446de400dce905e22f7
Evan <evan@evan-ThinkPad-P50>
2019-03-27 16:44:04 -0400

    Readds files with appropriate naming convention

---
+++ @classes/Link.m
@@ -0,0 +1,10 @@
+classdef Link
+    % Class that defines a single link in a network
+
+    properties
+        nodes % The nodes that this link connects
+        permitted_vehicles % Types of vehicles that can travel on this link
+
+
+    end
+end

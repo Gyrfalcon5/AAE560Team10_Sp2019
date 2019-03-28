be5d6e89310383758144fbdfb15e078b2f88e2ea
Evan <evan@evan-ThinkPad-P50>
2019-03-28 16:10:40 -0400

    Finishes first pass of node and link, as well as scenario setup in main

--- +classes/Link.m
+++ +classes/Link.m
@@ -1,4 +1,4 @@
-classdef Link
+classdef Link < handle
     % Class that defines a single link in a network
 
     properties
@@ -22,9 +22,10 @@ classdef Link
         function time = get.travel_time(obj)
             time = travel_fun(num_vehicles);
         end
-
+        
+        % Constructor
         function obj = Link(nodes, permitted_vehicles, num_vehicles, travel_fun)
-
+            
             if nargin == 0
                 args{1} = [];
                 args{2} = {'all'};
@@ -37,13 +38,34 @@ classdef Link
                 args{4} = travel_fun;
             end
 
-            obj.nodes = args{4};
+            obj.nodes = args{1};
             obj.permitted_vehicles = args{2};
             obj.num_vehicles = args{3};
             obj.travel_fun = args{4};
 
         end
-
-
+        
+        % Plots individual node
+        function plotLink(obj)
+            x(1) = obj.nodes(1).coordinate(1);
+            x(2) = obj.nodes(2).coordinate(1);
+            y(1) = obj.nodes(1).coordinate(2);
+            y(2) = obj.nodes(2).coordinate(2);
+            plot(x, y, 'b');
+        end
+        
+        % Plots many nodes quickly
+        function bulkLinkPlot(objs)
+            nodes = [objs.nodes];
+            starts = [nodes(1:2:end).coordinate]; 
+            ends = [nodes(2:2:end).coordinate];
+            start_x = starts(1:2:end);
+            end_x = ends(1:2:end);
+            start_y = starts(2:2:end);
+            end_y = ends(2:2:end);
+            x = [start_x ; end_x];
+            y = [start_y ; end_y];
+            plot(x, y, 'b')
+        end
     end
 end
--- Main.m
+++ Main.m
@@ -7,12 +7,65 @@
 clc, clear, close all;
 import classes.*
 
+
 %% Initialize Simulation
 
-num_links = 100;
-link_array(num_links) = Link;
-for idx = num_links:-1:1
-    link_array(idx) = Link;
+figure;
+hold on;
+% Creat and plot nodes
+x_dim = 10;
+y_dim = 10;
+map(x_dim, y_dim) = Node;
+for idx = 1:x_dim
+    for jdx = 1:y_dim
+        map(idx, jdx) = Node;
+        map(idx, jdx).coordinate = [idx jdx];
+        plotNode(map(idx, jdx))
+    end
 end
 
-link_array
+
+% Create links between the nodes
+links = {};
+for idx = 1:x_dim
+    for jdx = 1:y_dim
+
+        if idx+1 <= x_dim
+            
+            links{2*idx - 1}(jdx) = Link([map(idx, jdx) map(idx+1, jdx)],...
+                                         {'all'}, 0, @(x) 1);
+            plot([map(idx, jdx).coordinate(1), map(idx+1, jdx).coordinate(1)],...
+                 [map(idx, jdx).coordinate(2), map(idx+1, jdx).coordinate(2)],...
+                 'r');
+
+        end
+
+        if jdx+1 <= y_dim
+
+            links{2*idx}(jdx) = Link([map(idx, jdx) map(idx, jdx+1)],...
+                                     {'all'}, 0, @(x) 1);
+            plot([map(idx, jdx).coordinate(1), map(idx, jdx+1).coordinate(1)],...
+                 [map(idx, jdx).coordinate(2), map(idx, jdx+1).coordinate(2)],...
+                 'b');
+
+        end
+    end
+end
+
+% Make sure all the nodes know what links they have
+for idx = 1:length(links)
+    for jdx = 1:length(links{idx})
+        nodes = links{idx}(jdx).nodes;
+
+        for kdx = 1:length(nodes)
+            nodes(kdx).links{end+1} = links{idx}(jdx);
+        end
+    end
+end
+
+
+%bulkNodePlot(map(:))
+%bulkLinkPlot([links{:}])
+
+axis([0 x_dim+1 0 y_dim+1])
+axis equal;

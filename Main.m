792b93abe5e0ef929683596ef0b0ceacc1784df7
Evan <evan@evan-ThinkPad-P50>
2019-03-31 17:17:16 -0400

    Adds support for multiple cars on the animation

--- Main.m
+++ Main.m
@@ -10,11 +10,13 @@ import classes.*
 
 %% Initialize Simulation
 
-figure;
+figure('units','normalized','outerposition',[0 0 1 1]);
+axis equal;
 hold on;
 % Creat and plot nodes
 x_dim = 10;
 y_dim = 10;
+axis([0 x_dim+1 0 y_dim+1])
 map(x_dim, y_dim) = Node;
 for idx = 1:x_dim
     for jdx = 1:y_dim
@@ -67,71 +69,160 @@ end
 % Vehicle
 x_v = 7;
 y_v = 7;
+
+% Making cars
+num_cars = 100;
+cars = {};
+for idx = num_cars:-1:1
+    cars{idx} = Vehicle;
+    cars{idx}.coordinate = [randi([1,x_dim]) randi([1,y_dim])];
+    cars{idx}.onNode = 1;
+    cars{idx}.onLink = 0;
+end
+
+
+
 while(1)
-%making a car
-green_car = Vehicle;
-green_car.coordinate = [x_v,y_v];
-green_car.onNode = 1;
-green_car.onLink = 0;
+    
+    
+    currentMapNodeCoordinates = zeros(num_cars, 2);
+    currentMapNodeLinks = {};
+    numberOfLinks = [];
+    randomPath = [];
+    nextLinkNodes = {};
+    a = {};
+    b = {};
+    x = {};
+    y = {};
+    hPlot = {};
+    for idx = 1:num_cars
+        
+        currentMapNodeCoordinates(idx, :) = cars{idx}.coordinate;
+        currentMapNodeLinks{idx} = map(currentMapNodeCoordinates(idx, 1), currentMapNodeCoordinates(idx, 2)).links;
+        numberOfLinks = length(currentMapNodeLinks{idx});
+        
+        randomPath(idx) = randi([1,numberOfLinks]);
+        chosenLink = map(currentMapNodeCoordinates(idx, 1),currentMapNodeCoordinates(idx, 2)).links{randomPath(idx)};
+        nextLinkNodes{idx} = chosenLink.nodes;
+        
+        if(nextLinkNodes{idx}(1).coordinate == [currentMapNodeCoordinates(idx, 1),currentMapNodeCoordinates(idx, 2)])
+            startNode = nextLinkNodes{idx}(1);
+            endNode = nextLinkNodes{idx}(2);
+        else
+            startNode = nextLinkNodes{idx}(2);
+            endNode = nextLinkNodes{idx}(1);
+        end
+        
+        
+        % %a and b is used so i dont have to type such long code :)
+        a{idx} = startNode.coordinate;
+        b{idx} = endNode.coordinate;
 
-%getting info on starting cords
-currentMapNodeCoordinates = map(x_v,y_v).coordinate;
-currentMapNodeLinks = map(currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)).links;
+        % determine the x values
+        x{idx} = linspace(a{idx}(1),b{idx}(1),100);
 
-%getting the number of links
-clength = size(currentMapNodeLinks);
-numberOfLinks = clength(2);
+        % determine the y values
+        y{idx} = linspace(a{idx}(2),b{idx}(2),100);
 
-%random number, so to travel
-randomPath = randi([1,numberOfLinks],1,1);
+        % get a handle to a plot graphics object
 
-%get the nodes of the choosen link
-choosenlink = map(currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)).links{1,randomPath};
-nextLinkNodes = map(currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)).links{1,randomPath}.nodes;
+        %ploting a car
 
-%getting the end node and the starting node for current link
-if(nextLinkNodes(1).coordinate == [currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)])
-    startNode = nextLinkNodes(1);
-    endNode = nextLinkNodes(2);
-else
-    startNode = nextLinkNodes(2);
-    endNode = nextLinkNodes(1);
-end
+        %plot(x,y)
+        %plotVehicle(green_car,x,y);
 
-%a and b is used so i dont have to type such long code :)
-a = startNode.coordinate;
-b = endNode.coordinate;
+        hPlot{idx} = plot(NaN,NaN,'ko', 'Markerfacecolor', 'k');
+        
+    end
+    
+    
+    for idx = 1:length(x{1})
+        
+        for jdx = 1:num_cars
+           
+            set(hPlot{jdx},'XData',x{jdx}(idx),'YData',y{jdx}(idx));
+            if(idx == (length(x{1})))
+                set(hPlot{jdx},'XData',-1,'YData',-1);
+            end
+            
+            
+        end
+        
 
-% determine the x values
-x = linspace(a(1),b(1),500);
-
-% determine the y values
-y = linspace(a(2),b(2),500);
-
-% get a handle to a plot graphics object
-
-%ploting a car
-
-%plot(x,y)
-%plotVehicle(green_car,x,y);
-
-hPlot = plot(NaN,NaN,'go');
-for k=1:length(x)
-     % update the plot graphics object with the next position
-     set(hPlot,'XData',x(k),'YData',y(k));
-
-     % pause for 0.5 seconds
-     pause(.001);
-     
-     %to stop the plot from leaving behind phantom cars. very spooooky
-     if(k == (length(x)-1))
-         hPlot = plot(NaN,NaN,'bo');
-         set(hPlot,'XData',1,'YData',1);
-     end
-end
- 
-x_v = b(1);
-y_v = b(2);
+        
+        pause(0.000001)
+    
+    end
+    
+    for idx = 1:num_cars
+        cars{idx}.coordinate = b{idx};
+    end
+    
+    
+% %making a car
+% green_car = Vehicle;
+% green_car.coordinate = [x_v,y_v];
+% green_car.onNode = 1;
+% green_car.onLink = 0;
+% 
+% %getting info on starting cords
+% currentMapNodeCoordinates = map(x_v,y_v).coordinate;
+% currentMapNodeLinks = map(currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)).links;
+% 
+% %getting the number of links
+% clength = size(currentMapNodeLinks);
+% numberOfLinks = clength(2);
+% 
+% %random number, so to travel
+% randomPath = randi([1,numberOfLinks]);
+% 
+% %get the nodes of the choosen link
+% choosenlink = map(currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)).links{1,randomPath};
+% nextLinkNodes = map(currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)).links{1,randomPath}.nodes;
+% 
+% %getting the end node and the starting node for current link
+% if(nextLinkNodes(1).coordinate == [currentMapNodeCoordinates(1),currentMapNodeCoordinates(2)])
+%     startNode = nextLinkNodes(1);
+%     endNode = nextLinkNodes(2);
+% else
+%     startNode = nextLinkNodes(2);
+%     endNode = nextLinkNodes(1);
+% end
+% 
+% %a and b is used so i dont have to type such long code :)
+% a = startNode.coordinate;
+% b = endNode.coordinate;
+% 
+% % determine the x values
+% x = linspace(a(1),b(1),250);
+% 
+% % determine the y values
+% y = linspace(a(2),b(2),250);
+% 
+% % get a handle to a plot graphics object
+% 
+% %ploting a car
+% 
+% %plot(x,y)
+% %plotVehicle(green_car,x,y);
+% 
+% hPlot = plot(NaN,NaN,'go');
+% for k=1:length(x)
+%      % update the plot graphics object with the next position
+%      set(hPlot,'XData',x(k),'YData',y(k));
+% 
+%      % pause for 0.5 seconds
+%      pause(.001);
+%      
+%      %to stop the plot from leaving behind phantom cars. very spooooky
+%      if(k == (length(x)-1))
+%          hPlot = plot(NaN,NaN,'bo');
+%          set(hPlot,'XData',1,'YData',1);
+%      end
+% end
+%  
+% x_v = b(1);
+% y_v = b(2);
 end
 
 %bulkNodePlot(map(:))
